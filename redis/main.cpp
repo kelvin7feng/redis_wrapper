c70d4398409c473fb6ec03b2b9e611410d69fe45
kelvin7feng <kelvin7.feng@gmail.com>
2016-10-31 22:03:13 +0800

    Initial Commit

---
+++ redis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  redis
+//
+//  Created by 冯文斌 on 16/10/31.
+//  Copyright © 2016年 kelvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
