4739ce1f872e5bb0f664bd5cce9b038c078817e6
kelvin7feng <kelvin7.feng@gmail.com>
2019-08-17 05:18:17 -0400

    cmake

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/dev/redis_wrapper/class/redis_client.cpp" "/opt/dev/redis_wrapper/build/CMakeFiles/main.dir/class/redis_client.cpp.o"
+  "/opt/dev/redis_wrapper/redis/main.cpp" "/opt/dev/redis_wrapper/build/CMakeFiles/main.dir/redis/main.cpp.o"
+  "/opt/dev/redis_wrapper/test/redis_test.cpp" "/opt/dev/redis_wrapper/build/CMakeFiles/main.dir/test/redis_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../hiredis"
+  "../class"
+  "../test"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/opt/dev/redis_wrapper/build/class/CMakeFiles/class.dir/DependInfo.cmake"
+  "/opt/dev/redis_wrapper/build/test/CMakeFiles/test.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/class/redis_client.cpp.o"
+  "CMakeFiles/main.dir/redis/main.cpp.o"
+  "CMakeFiles/main.dir/test/redis_test.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
